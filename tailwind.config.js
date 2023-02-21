5590a5c0fe3b6eb2a14ac62494c07b782fed7fa7
Felipe <fcas@hey.com>
2023-02-21 04:44:57 -0500

    new year new site

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#f4f4f5',
+        'theme': '#000000',
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
